2bd070ea4c16f3064b2c7b3e9eb474e2c789b0ad
ryandornan <116069884+ryandornan@users.noreply.github.com>
2024-02-28 19:55:28 +0000

    Added random domain generator functionality.

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+  let randomPronounIndex = Math.floor(Math.random() * pronouns.length);
+  let randomAdjectiveIndex = Math.floor(Math.random() * adjectives.length);
+  let randomNounIndex = Math.floor(Math.random() * nouns.length);
+
+  let randomPronoun = pronouns[randomPronounIndex];
+  let randomAdjective = adjectives[randomAdjectiveIndex];
+  let randomNoun = nouns[randomNounIndex];
+
+  let randomDomain = randomPronoun + randomAdjective + randomNoun + ".com";
   console.log("Hello Rigo from the console!");
+
+  return randomDomain;
 };
+
+let domainElement = document.getElementById("domain");
+
+domainElement.textContent = generateRandomDomain();
